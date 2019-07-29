201c85dd9f6965ddd682a6bb85654acb0075dc49
ggon <bluekain@naver.com>
2019-07-29 14:52:10 +0900

    start project

---
+++ DarleneJ/src/main/java/com/ggon/darleneJ/DarleneJApplication.java
@@ -0,0 +1,13 @@
+package com.ggon.darleneJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarleneJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarleneJApplication.class, args);
+	}
+
+}
---
+++ DarleneJ/src/test/java/com/ggon/darleneJ/DarleneJApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ggon.darleneJ;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DarleneJApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
