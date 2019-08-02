e358eaa9ad9913d1519aa972b2fb0639ce0c9906
ggon <bluekain@naver.com>
2019-08-02 16:51:51 +0900

    Add User Mapper

---
+++ DarleneJ/src/main/java/com/ggon/darleneJ/user/port/adapter/persistence/IUserMapper.java
@@ -0,0 +1,22 @@
+package com.ggon.darleneJ.user.port.adapter.persistence;
+
+import java.util.List;
+//import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import com.ggon.darleneJ.user.domain.User;
+
+@Mapper
+public interface IUserMapper {
+	public List<User> login(@Param("email") String email, @Param("pwd") String pwd);
+	 
+    //public User selectEmployer(User empNo);
+ 
+    //public void insertEmployer(User vo);
+ 
+   // public int updateEmployer(User vo);
+ 
+    //public int deleteEmployer(User empNo);
+
+}
